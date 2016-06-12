85eadba67b3bec0054f74d7b1a9d7794c9e56714
Vennus <ccencho.monica@gmail.com>
2016-06-12 04:10:48 -0500

    terminando

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*SCROLL NAVBAR -HOME*/
+$(document).scroll(function(){
+    var scroll = $(this).scrollTop();
+    if(scroll >= 100){
+        $("#menu").addClass("active");
+    } else{
+        $("#menu").removeClass("active");
+    }
+});
+
+$(".navbar-toggle").click(function(){
+    $(this).toggleClass("open");
+})
